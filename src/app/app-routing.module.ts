50ffebb1fa0a9435a1b683a205b3424d80ae3d90
Vignesh-R-3kt <vignesh.r@3ktechnologies.com>
2024-03-12 12:42:29 +0530

    added auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { MainBodyComponent } from './components/main-body/main-body.component';
+import { authGuardGuard } from './shared/auth-guard.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'main-body', loadChildren: () => import('./components/main-body/main-body.module').then(m => m.MainBodyModule) },
+  { path: 'main-body', loadChildren: () => import('./components/main-body/main-body.module').then(m => m.MainBodyModule), canActivate: [authGuardGuard] },
   { path: '**', redirectTo: 'login' }
 ];
 
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,19 @@
 import { GoogleAuthService } from './../../services/google-auth.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
 
   constructor(private googleAuth: GoogleAuthService) { }
+
+  ngOnInit(): void {
+    sessionStorage.clear();
+  }
 
   login() {
     this.googleAuth.login();
