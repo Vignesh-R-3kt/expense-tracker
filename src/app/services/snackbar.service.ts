efcdcb6bc1e5ab7fde3653ce171b9d82f253df55
Vignesh-R-3kt <vignesh.r@3ktechnologies.com>
2024-03-21 10:50:49 +0530

    changes

--- src/app/services/snackbar.service.ts
+++ src/app/services/snackbar.service.ts
@@ -10,7 +10,7 @@ export class SnackbarService {
 
   success(message: string) {
     this.snackbar.open(message, 'dismiss', {
-      duration: 20000,
+      duration: 2000,
       horizontalPosition: 'end',
       verticalPosition: 'top',
       panelClass: 'success-message'
